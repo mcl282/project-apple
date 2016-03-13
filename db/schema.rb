62e1966eaef998bfcea1074a6072a5deb0e5dc4e
Michael Lee <mcl282@gmail.com>
2016-03-13 00:06:32 +0000

    add devise for tenant and property managers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228191156) do
+ActiveRecord::Schema.define(version: 20160229001250) do
 
   create_table "property_managers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,23 @@ ActiveRecord::Schema.define(version: 20160228191156) do
 
   add_index "property_managers", ["email"], name: "index_property_managers_on_email", unique: true
   add_index "property_managers", ["reset_password_token"], name: "index_property_managers_on_reset_password_token", unique: true
+
+  create_table "tenants", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "tenants", ["email"], name: "index_tenants_on_email", unique: true
+  add_index "tenants", ["reset_password_token"], name: "index_tenants_on_reset_password_token", unique: true
 
 end
