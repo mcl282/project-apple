94e3f2bcf205cd77c1cde285b27259b2274827f0
Michael Lee <mcl282@gmail.com>
2016-03-16 13:02:26 +0000

    add in code for location log

--- app/controllers/manager/sessions_controller.rb
+++ app/controllers/manager/sessions_controller.rb
@@ -1,25 +1,33 @@
 class Manager::SessionsController < Devise::SessionsController
-# before_filter :configure_sign_in_params, only: [:create]
+ before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+   def new
+     super
+   end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   def create
+     super do |resource|
+       new_location_log(resource)
+     end
+   end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+   def destroy
+     super
+   end
 
-  # protected
+   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
-  # end
+   def configure_sign_in_params
+     devise_parameter_sanitizer.for(:sign_in) << :attribute
+   end
+   
+   def new_location_log(resource)
+     ip = resource.current_sign_in_ip
+     id = resource.id
+     LocationLog.create(id: id, ip_address: ip)
+   end
 end
